4023a45f23df865efdf4641153d3bb0fd3900eb4
kangilc <kangilc@javaf.net>
2026-06-16 18:51:07 +0900

    style: increase footer logo size and remove opacity styling

--- frontend-user/src/App.tsx
+++ frontend-user/src/App.tsx
@@ -115,7 +115,7 @@ export const App: React.FC = () => {
 
             {/* Footer */}
             <footer className="py-6 flex flex-col items-center gap-2 text-center text-[10px] text-slate-500 font-semibold border-t border-white/5 bg-[#070b15]">
-                <img src="/JavaF_logo_tiny_400.png" alt="JavaF Logo" className="h-5 opacity-40 hover:opacity-70 transition-opacity object-contain" />
+                <img src="/JavaF_logo_tiny_400.png" alt="JavaF Logo" className="h-8 object-contain" />
                 <span>© 2026 JavaF Exchange. All rights reserved.</span>
             </footer>
 
